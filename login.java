ea44dfb16fc8c525baa95b7c5835d7243969b3f6
Niharsonu <35405603+Niharsonu@users.noreply.github.com>
2021-11-19 00:18:45 +0530

    Add files via upload

---
+++ login.java
@@ -0,0 +1,10 @@
+package login_bank;
+
+public class login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("print from GIT Prasanta");
+	}
+
+}
